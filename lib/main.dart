f5c0e57ff09e22c4f65a7178bccc43a3d839a698
Benjamin JALON <bjalon@qastia.com>
2022-03-24 06:09:26 +0100

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Text("Hello world !"),
+      ),
+    );
+  }
+}
